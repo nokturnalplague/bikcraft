8e134a177247b12b1982deec718d8230d3e0eb28
nokturnalplague <nathaliadagort@gmail.com>
2025-05-19 17:05:23 -0300

    faq

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,3 +28,22 @@ function ativarProduto(parametro) {
 
 parametros.forEach(ativarProduto)
 
+//FAQ
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPerguntas(event) {
+  const pergunta = event.currentTarget
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
+
+  resposta.classList.toggle('ativo')
+  const ativo = resposta.classList.contains('ativo')
+  pergunta.setAttribute('aria-expanded', ativo)
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPerguntas)
+}
+
+perguntas.forEach(eventosPerguntas)
