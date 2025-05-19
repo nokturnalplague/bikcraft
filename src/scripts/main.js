a0acf6b9c3800fe9d57fe7d8da0f6ee13c7be07a
nokturnalplague <nathaliadagort@gmail.com>
2025-05-19 15:47:30 -0300

    selecionar items no orçamento automaticamente

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 console.log('nokturnalplague')
+
+//Indicar que está na página pelo menu de navegação
+
+const links = document.querySelectorAll('.header-menu a')
+
+function ativarLink(link) {
+  const url = location.href
+  const href = link.href
+
+  if (url.includes(href)) {
+    link.classList.add('ativo')
+  }
+}
+
+links.forEach(ativarLink)
+
+//Selecionar items no orçamento automaticamente
+
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro) {
+  const element = document.getElementById(parametro)
+  if (element) {
+    element.checked = true
+  }
+}
+
+parametros.forEach(ativarProduto)
+
